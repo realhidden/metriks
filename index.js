c6979d328285c3d179d768814c72262f35e1de95
Zsombor Paroczi <zsombor.paroczi@gmail.com>
2017-12-26 20:27:04 +0100

    munin integration with pure data extraction
    + use dotenv in config
    + some extra cleanup

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-exports         = module.exports = Metriks;
-exports.Metriks = Metriks;
+const MuninNode = require('./lib/munin-node');
 
-function Metriks(config) {
-  this.config        = config;
-  this.pluginManager = new PluginManager(this.config);
+//dotenv config
+require('dotenv').config();
+
+var workDir = process.env.WORK_DIR || '.';
+var options = {
+    //If > 0, run a webserver on that port to browse graphs
+    webPort: parseInt(process.env.WEB_PORT) || 0,
+    //How many hosts to run at once
+    concurrency: process.env.CONCURRENCY || 5,
+    //Automatically write png files to png-dir
+    autoWritePng: process.env.AUTO_PNG || 0,
+    //RRD directory. Overrules workDir
+    rrdDir: workDir + '/rrd',
+    //Image / HTML directory. Overrules workDir
+    pngDir: workDir + '/png',
+    //update interval (5 minutes default)
+    updateInterval: process.env.UPDATE_INTERVAL || 1000 * 60 * 5
 }
 
-var PluginManager = require('./lib/plugin-manager').PluginManager;
-var WebServer     = require('./lib/web-server').WebServer;
+class Metriks {
+    constructor(config) {
+        this.config = config;
+        this.nodes = {};
+        this.updatetimeout = null;
+    }
 
+    start() {
+        this.nodes['test'] = new MuninNode({
+            ip: '192.168.43.193',
+            port: 4949
+        });
 
-Metriks.prototype.start = function() {
-};
+        if (this.config.webPort > 0) {
+            var WebServer = require('./lib/web-server').WebServer;
+            var webServer = new WebServer(this.config);
+            console.log(webServer);
+            webServer.start();
+        }
 
-Metriks.prototype.start = function() {
-  
-  if (this.config.graph) {
-    this.pluginManager.graph(program.graph);
-  } else {
-    this.pluginManager.start();
-  }
+        //this.updatetimeout = setTimeout(this.periodicupdate.bind(this), this.config.updateInterval)
+        this.periodicupdate();
+    }
 
-  if (this.config.webPort > 0) {
-    var webServer = new WebServer(this.config);
-    webServer.start();
-  }
-};
+    periodicupdate() {
+        //clearTimeout(this.updatetimeout);
+        Object.keys(this.nodes).forEach(function (item) {
+            console.log("Updating " + item);
+            this.nodes[item].update(() => {
+                console.log("Node processing finished")
+            });
+        }.bind(this));
+    }
+}
+
+const metriks = new Metriks(options);
+metriks.start();
